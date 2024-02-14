c2a8709f17032beb9f2e25e34ae12518c6923388
Dakshh <dakshhsingh4902@gmail.com>
2024-02-14 20:50:14 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 dotenv.config();
 
@@ -10,16 +13,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!!!");
 });
 
-app.get("/api/auth/signup", (req, res) => {
-    console.log("signup route");
-});
-
-app.get("/api/auth/login", (req, res) => {
-    console.log("login route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-    console.log("logout route");
-});
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
