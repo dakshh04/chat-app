e17da84dbc77fe00d4cee631a4322c561f08f1b7
Dakshh <dakshhsingh4902@gmail.com>
2024-02-15 21:03:37 +0530

    modified

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 export const signup = async (req, res) => {
     try {
         const {fullName,username,password,confirmPassword,gender} = req.body;
-    } catch (error) {
-        
-    }
+    } catch (error) {}
 }
 
 export const login = (req, res) => {
