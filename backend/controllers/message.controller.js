159657d6204a2531769abdf67991e481ceb6b088
Dakshh <dakshhsingh4902@gmail.com>
2024-02-20 21:41:57 +0530

    modified

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,10 +1,17 @@
+import Conversation from "../models/conversation.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
-    const {message} = req.body;
-    const {id} = req.params;
-    const senderId = req.userId;
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    })
+
   } catch (error) {
-    console.log("Error in sendMessage controller: ", error.message)
+    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
