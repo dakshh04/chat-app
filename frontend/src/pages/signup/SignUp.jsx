fd535b60c49bedb527f75eef374c4e7594bdf67e
Dakshh <dakshhsingh4902@gmail.com>
2024-03-01 00:06:10 +0530

    modified

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -15,9 +15,9 @@ const SignUp = () => {
     setInputs({ ...inputs, gender });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputs);
+    await signup(inputs)
   };
 
   return (
