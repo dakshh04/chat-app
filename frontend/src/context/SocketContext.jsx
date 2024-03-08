ece45fd0f3e2cdaeedf382d3720c812e5174d4da
Dakshh <dakshhsingh4902@gmail.com>
2024-03-08 22:56:19 +0530

    modified

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,8 +1,12 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { useAuthContext } from "./AuthContext";
 import io from "socket.io-client";
 
 export const SocketContext = createContext();
+
+export const useSocketContext = () => {
+	return useContext(SocketContext);
+};
 
 export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
@@ -11,9 +15,17 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000");
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id,
+        },
+      });
 
       setSocket(socket);
+
+      socket.on("getOnlineUsers", (users) => {
+				setOnlineUsers(users);
+			});
 
       return () => socket.close();
     } else {
@@ -22,7 +34,11 @@ export const SocketContextProvider = ({ children }) => {
         setSocket(null);
       }
     }
-  }, []);
+  }, [authUser]);
 
-  return <SocketContext.Provider value={{socket, onlineUsers}}>{children}</SocketContext.Provider>;
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
